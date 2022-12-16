849f634314d7791cf845567657eb983061fe4ab6
mach_j <mach_j@etna-alterance.net>
2022-12-16 05:23:27 -0500

    route get publi  ok

--- frontend/src/components/LayoutPost.vue
+++ frontend/src/components/LayoutPost.vue
@@ -4,24 +4,7 @@
       <div class="post__avatar">
         <img src="../assets/icon/logo.webp" alt="" class="post-avatar__img" />
       </div>
-      <div class="post__main post-main">
-        <div class="post-main__user mb-3">
-          <span class="post-main__username mr-3">Username</span>
-          <span class="post-main__nickname mr-3">@nickName</span>
-          <span class="post-main__date mr-3">28.01.2021</span>
-          <img class="post-delete" src="../assets/icon/x-solid.svg" />
-        </div>
-        <div class="post-main__text mb-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id placeat
-          obcaecati nisi accusamus, vitae esse!
-        </div>
-        <div class="post-main__image mb-2">
-          <img
-            src="../assets/img_wallpaper/login.jpg"
-            alt=""
-            class="post-main__img"
-          />
-        </div>
+    
         <div id="containerpost"></div>
 
         <div class="post-main__bottom">
@@ -30,7 +13,7 @@
         </div>
       </div>
     </div>
-  </div>
+  
 </template>
 
 <script>
