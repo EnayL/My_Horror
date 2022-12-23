340cdc5691d97072049ea189a46277ea2b084603
sasees_a <sasees_a@etna-alternance.net>
2022-12-23 12:17:08 +0100

    save

--- frontend/src/components/PostId.vue
+++ frontend/src/components/PostId.vue
@@ -1,16 +1,13 @@
 <template class="body">
-  <div>
-    </div>
+  <div></div>
   <div class="bodyhome">
     <main class="main">
       <post></post>
       <div class="main-line2"></div>
       <div class="main-form">
-      <create-post></create-post>
+        <create-post></create-post>
       </div>
     </main>
-
-    
   </div>
 </template>
 
@@ -30,38 +27,35 @@ export default {
 };
 </script>
 
-<style >
-.bodyhome{
+<style>
+.bodyhome {
   background-image: url("../assets/back.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   height: 100vh;
- background-attachment: fixed;
-
+  background-attachment: fixed;
 }
 
 .main {
-  
- padding: 5%;
- background-color: rgba(0,0,0, 0.5);
- width: 60%;
- min-height: 100%;
- max-height: fit-content;
- margin-left: auto;
- margin-right: auto;
- overflow: hidden;
- overflow: auto;
+  padding: 5%;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 60%;
+  min-height: 100%;
+  max-height: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+  overflow: hidden;
+  overflow: auto;
 }
-
 
 .main-form {
   margin-top: 10px;
   padding: 10px;
-} 
+}
 
 .main-line2 {
   height: 5px;
   background-color: rgb(139, 139, 139);
   width: 100%;
-} 
+}
 </style>
