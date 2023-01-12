3007625084b95a55694e07e6f851add1e7d730e9
sasees_a <sasees_a@etna-alternance.net>
2023-01-12 12:57:01 +0100

    header

--- frontend/src/components/LayoutPost.vue
+++ frontend/src/components/LayoutPost.vue
@@ -1,13 +1,11 @@
 <template>
-    <a href="./home" style="color: white;"> GO BACK HOME</a>
-    <div id="containerpost">
-    </div>  
+  <a href="./home" style="color: white"> GO BACK HOME</a>
+  <div id="containerpost"></div>
 </template>
 
 <script>
 import axios from "axios";
 import LayoutHeader from "./LayoutHeader.vue";
-
 
 export default {
   methods: {
@@ -17,16 +15,13 @@ export default {
       console.log(finalRes);
     },
 
-    
-
     async getPosts() {
       //récupération des données
-      const token = localStorage.getItem("token")
-      const res = await axios.get("http://localhost:3000/posts/",{
-        
-        headers:{
-           "Authorization":`Bearer ${token}`
-        }
+      const token = localStorage.getItem("token");
+      const res = await axios.get("http://localhost:3000/posts/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
       const json = JSON.stringify(res);
       const jsonData = JSON.parse(json);
@@ -38,8 +33,7 @@ export default {
         const contenu = publi.contenu;
         const owner = publi.owner;
 
-        var container = document.createElement("div"); 
-        
+        var container = document.createElement("div");
 
         var pp = document.createElement("img"); // pp
         pp.src = "../assets/icon/heart-regular.svg";
@@ -47,20 +41,18 @@ export default {
         var div = document.createElement("div"); // div de la pp
 
         var h1 = document.createElement("p"); // creation element titre
-        h1.innerHTML = titre+":";
+        h1.innerHTML = titre + ":";
         var div2 = document.createElement("p"); // creation element contenu
-        
+
         div2.innerHTML = contenu;
         var div3 = document.createElement("p"); //creation element owner
         div3.class = "owner";
-        div3.innerHTML = "Par "+ owner;
+        div3.innerHTML = "Par " + owner;
         mainContainer.appendChild(container);
 
-        
         div2.setAttribute("style", "margin: 10px;");
         div3.setAttribute("style", "margin: 10px; font-size: x-small;");
         div.setAttribute("style", "display: flex; flex-direction: row;");
-        
 
         var supp = document.createElement("button"); // creation boutton supprimer
         supp.id = "supprimer";
@@ -78,57 +70,50 @@ export default {
           "style",
           "width:5%; margin-left:95%; padding: 5px; text-align: center; background-color: rgba(0,0,0,0); border: none; font-size: x-large;"
         );
-        const token = localStorage.getItem("token")
-        const user = localStorage.getItem("user")
-        supp.addEventListener('click', function handleClick() {
-          if (owner != user){
-            alert("Vous n'avez pas le droit de supprimer une publication qui n'est pas à vous.")
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+        supp.addEventListener("click", function handleClick() {
+          if (owner != user) {
+            alert(
+              "Vous n'avez pas le droit de supprimer une publication qui n'est pas à vous."
+            );
+          } else {
+            alert("vous avez supprimé une publication");
+            axios
+              .delete(`http://localhost:3000/posts/delete/?titre=${titre}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              })
+              .then((res) => {
+                console.log(titre);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           }
-          else {
-            alert("vous avez supprimé une publication")
-          axios.delete(`http://localhost:3000/posts/delete/?titre=${titre}`, {
-            headers:{
-              "Authorization":`Bearer ${token}`
-            }
-          })
-          .then(res => {
-            console.log(titre);
-          })
-          .catch(err => {
-              console.log(err);
-          });
-          }          
         });
 
         container.setAttribute(
-        'style',
-        'background-color: rgba(9,9,9, 0.5); min-height: 150px; margin: 15px; display:flex; flex-direction: column;',
+          "style",
+          "background-color: rgba(9,9,9, 0.5); min-height: 150px; margin: 15px; display:flex; flex-direction: column;"
         );
 
         h1.setAttribute(
-          'style',
-          'font-size: x-large; margin: 10px; text-decoration: underline dotted ;margin-right:auto; margin-left:auto;'
+          "style",
+          "font-size: x-large; margin: 10px; text-decoration: underline dotted ;margin-right:auto; margin-left:auto;"
         );
 
         supp.setAttribute(
-          'style',
-          'width:5%; margin-left:95%; padding: 5px; text-align: center; background-color: rgba(0,0,0,0); border: none; font-size: x-large;'
+          "style",
+          "width:5%; margin-left:95%; padding: 5px; text-align: center; background-color: rgba(0,0,0,0); border: none; font-size: x-large;"
         );
 
-        div2.setAttribute(
-          'style',
-          'margin: 10px;'
-        );
+        div2.setAttribute("style", "margin: 10px;");
 
-        div3.setAttribute(
-          'style',
-          'margin: 10px; font-size: x-small;'
-        );
+        div3.setAttribute("style", "margin: 10px; font-size: x-small;");
 
-        div.setAttribute(
-          'style',
-          'display: flex; flex-direction: row;'
-        );
+        div.setAttribute("style", "display: flex; flex-direction: row;");
 
         container.appendChild(div);
         container.appendChild(h1);
@@ -136,7 +121,6 @@ export default {
         div.appendChild(supp);
         container.appendChild(div2);
         container.appendChild(div3);
-
       }
     },
   },
@@ -154,7 +138,6 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;300&display=swap");
 /* style="cursor: pointer" */
 
-
 .post__avatar {
   margin-right: 20%;
 }
@@ -167,5 +150,4 @@ export default {
   border: 1px solid rgba(255, 255, 255, 0.04);
   box-shadow: rgba(0, 0, 0, 0.02) 0px 0px 2px inset;
 }
-
 </style>
